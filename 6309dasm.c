d03cb6cc44a976ca7e2334df8015d696265f7dfc
strick <strick@yak.net>
2022-12-23 00:55:06 -0800

    print branch offset in decimal, so it's easy to count bytes.

--- 6309dasm.c
+++ 6309dasm.c
@@ -671,7 +671,7 @@ extern void add_code_label( int address );
 
 // XXX
 void add_code_label( int address ) {
-  printf("\n*** add_code_label(%x)\n", address);
+  printf("*** add_code_label:  %d decimal.\n", (int)(short)address);
 }
 
 unsigned Dasm6309 (char *buffer, int pc, unsigned char *memory, size_t memory_size)
